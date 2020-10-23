62d775582ce0f6f98b302d46a2e0b6dccfa94c55
Stepan Khvatov <stepanhv@yandex.ru>
2020-10-23 11:13:57 +0700

    jwt fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
+const { JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.headers.authorization && req.headers.authorization.replace('Bearer ', '');
 
   let payload;
 
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new UnauthorizedError({ message: 'Необходима авторизация' }); // Ошибка 401
   }
